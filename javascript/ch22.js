a34fdac80440662fab01629d60f9971b15e59728
ch2ch2 <116795075+ch2ch2jo@users.noreply.github.com>
2022-11-23 23:09:33 +0900

    Add files via upload

---
+++ javascript/ch22.js
@@ -0,0 +1,9 @@
+
+// faq js
+// $(function(){
+//     $(".listWrap .qa_li .question").click(function(){
+//     $(this).next(".answer").slideToggle(200);
+//     $(this).parent("li").siblings(".answer").children("p").slideUp(200);
+//     });
+//   });
+
