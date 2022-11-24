98eadb8082e7f1a027c7ee0e79371a653696397d
qkr5318 <qkr5318@gmail.com>
2022-11-25 01:09:33 +0900

    recommit

--- javascript/ch22.js
+++
@@ -1,9 +0,0 @@
-
-// faq js
-// $(function(){
-//     $(".listWrap .qa_li .question").click(function(){
-//     $(this).next(".answer").slideToggle(200);
-//     $(this).parent("li").siblings(".answer").children("p").slideUp(200);
-//     });
-//   });
-
